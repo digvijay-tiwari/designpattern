20eacb6538807821465cb016280a6cf923ae23ec
Digvijay <digvijaykrtiwari@gmail.com>
2024-03-12 16:38:11 +0530

    factory

--- AbstractFactory/Factories/Employeefactory.php
+++ AbstractFactory/Factories/Employeefactory.php
@@ -8,14 +8,32 @@ use Abstractfactory\Factories\Developerfactory;
 use Abstractfactory\Factories\Managerfactory;
 
 class EmployeeFactory {
-    public function getDevSalaries() {
+    public function getDevDetails() {
         $developer = new Developerfactory();
         $devData = $developer->createEmployee();
-        return $devData->getSalary();
+        //return $devData->getSalary();
+        return [
+            'dev' => [
+                'Message' => $devData->getName(),
+                'Salary' => $devData->getSalary()
+            ]
+        ];
+    }
+    public function getmanagersDetails() {
+        $manager = new Managerfactory();
+        $mgrData = $manager->createEmployee();
+        //return $devData->getSalary();
+        return [
+            'mgr' => [
+                'Message' => $mgrData->getName(),
+                'Salary' => $mgrData->getSalary()
+            ]
+        ];
     }
 }
 
 
 $employee = new EmployeeFactory();
-echo $employee->getDevSalaries();
+print_r($employee->getDevDetails());
+print_r($employee->getmanagersDetails());
 
--- AbstractFactory/Factories/ManagerFactory.php
+++ AbstractFactory/Factories/ManagerFactory.php
@@ -5,7 +5,7 @@ require_once "../Interfaces/EmployeeFactoryInterface.php";
 require_once "../Manager.php";
 
 use Abstractfactory\Interfaces\EmployeeFactory;
-use Employees\Manager;
+use Abstractfactory\Manager;
 
 class Managerfactory implements EmployeeFactory {
     public function createEmployee()
